26987361dad98fa10477d93e62129d292a4ff640
BlaiseMoses01 <97707627+BlaiseMoses01@users.noreply.github.com>
2022-01-23 18:28:48 -0500

    Update Observer.java

--- CSCE247/design_patterns/observer/Observer.java
+++ CSCE247/design_patterns/observer/Observer.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
-//Written by Blaise Moses
-
+/**
+*interface for Observer classes, requires the update method so that the vote totals and percentages are easily modifiable/updatable.
+*/
 public interface Observer{
     public void update(HashMap<String,Integer> votes);
 }
